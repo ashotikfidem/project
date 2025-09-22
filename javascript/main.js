aecb357c444e4d6b221e144f414417dbf319436c
ashotvardanyan <ash_077@icloud.com>
2025-09-22 10:22:18 +0400

    change paths

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,7 @@
-fetch("../../wrappers/header.html")
+fetch("wrappers/header.html")
     .then(res => res.text())
     .then(data => document.getElementById("header").innerHTML = data);
 
-fetch("../../wrappers/footer.html")
+fetch("wrappers/footer.html")
     .then(res => res.text())
     .then(data => document.getElementById("footer").innerHTML = data);
