1e9a5f4bcf94eceb21a3452ce3fa2c3ce6c0ed1e
ashotvardanyan <ash_077@icloud.com>
2025-09-23 16:59:18 +0400

    structure changes and return page layout

--- javascript/main.js
+++
@@ -1,7 +0,0 @@
-fetch("/project/wrappers/header.html")
-    .then(res => res.text())
-    .then(data => document.getElementById("header").innerHTML = data);
-
-fetch("/project/wrappers/footer.html")
-    .then(res => res.text())
-    .then(data => document.getElementById("footer").innerHTML = data);
